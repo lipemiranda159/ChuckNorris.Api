decd43aede04377c539f07d9504faa17fd94c962
Felipe Miranda <lipemiranda159@hotmail.com>
2022-06-13 20:58:36 -0300

    create services

---
+++ ChuckNorris.Models/JokesResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChuckNorris.Models
+{
+    public class JokesResponse
+    {
+        public List<object> categories { get; set; }
+        public string created_at { get; set; }
+        public string icon_url { get; set; }
+        public string id { get; set; }
+        public string updated_at { get; set; }
+        public string url { get; set; }
+        public string value { get; set; }
+    }
+}
---
+++ ChuckNorris.Services/IChuckNorrisApiService.cs
@@ -0,0 +1,12 @@
+﻿using ChuckNorris.Models;
+using RestEase;
+using System.Threading.Tasks;
+
+namespace ChuckNorris.Services
+{
+    public interface IChuckNorrisApiService
+    {
+        [Get]
+        Task<JokesResponse> GetJokesAsync();
+    }
+}

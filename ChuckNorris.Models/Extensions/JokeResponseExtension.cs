039b7daa5c578b4ca5acf7ddd0dd8c9d0dd07e04
Felipe Miranda <lipemiranda159@hotmail.com>
2022-06-13 22:36:47 -0300

    Create Joke Response extension to transform data

---
+++ ChuckNorris.Models/Extensions/JokeResponseExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace ChuckNorris.Models.Extensions
+{
+    public static class JokeResponseExtension
+    {
+        public static string ToJoke(this JokesResponse jokesResponse)
+        {
+            return jokesResponse.value.Replace("Chuck", "CH.");
+        }
+    }
+}

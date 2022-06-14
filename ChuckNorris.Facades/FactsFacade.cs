84288b9e9a98151acb4434adc049e4c56f8ba98f
Felipe Miranda <lipemiranda159@hotmail.com>
2022-06-13 22:36:23 -0300

    Implement IFacts facade

--- ChuckNorris.Facades/FactsFacade.cs
+++ ChuckNorris.Facades/FactsFacade.cs
@@ -1,4 +1,6 @@
 ﻿using ChuckNorris.Facades.Interfaces;
+using ChuckNorris.Models.Extensions;
+using ChuckNorris.Services;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -6,9 +8,20 @@ namespace ChuckNorris.Facades
 {
     public class FactsFacade : IFactsFacade
     {
-        public Task<List<string>> GetFactsAsync()
+        private readonly IChuckNorrisApiService _chuckNorrisApiService;
+        public FactsFacade(IChuckNorrisApiService chuckNorrisApiService)
         {
-            throw new System.NotImplementedException();
+            _chuckNorrisApiService = chuckNorrisApiService;
+        }
+        public async Task<List<string>> GetFactsAsync(int count)
+        {
+            var result = new List<string>();
+            for (int i = 0; i < count; i++)
+            {
+                var facts = await _chuckNorrisApiService.GetJokesAsync();
+                result.Add(facts.ToJoke());
+            }
+            return result;
         }
     }
 }
